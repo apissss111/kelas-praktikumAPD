fe86961c16a43d9bd1f6f92e4819a39106f26cad
taufiqulhfz <denok7575@gmail.com>
2024-10-16 16:23:59 +0800

    Add files via upload

---
+++ pertemuan7_2409106099_MuhammadTaufiqulHafizhMuttaqin.py
@@ -0,0 +1,124 @@
+# def teks ():
+#     print("""halo semuanya
+#           whfouehwhcjwh
+#           heliewhclwdhclwicehwhnjdcbhfejehh"")
+    
+# pilihan = 0
+# if pilihan == 1:
+#     salam()
+# elif pilihan == 2:
+#     salam()
+# elif pilihan == 3:
+#     salam()
+# elif pilihan == 4:
+#     salam()
+# elif pilihan == 5:
+#     salam()
+
+# def penjumlahan (a,b,c):
+#     hasil = a+b+c
+#     print(hasil)
+    
+# penjumlahan(3,6,8)
+
+# def penjumlahan():
+#     return 7 + 3
+
+# hasil = penjumlahan()
+# print(hasil)
+
+
+# def penjumlahan():
+# a = int(input ("bilangan pertama : "))
+# b = int(input ("bilangan kedua : "))
+# c = int(input ("bilangan ketiga : "))
+
+# def perkalian (a,b):
+#     hasil = a*b
+#     print(hasil)
+    
+# perkalian(5,3)
+
+#luas persegi
+# def luas_persegi(sisi):
+#     luas = sisi * sisi
+#     print(f"luas persegi = {luas}")
+    
+# sisi = int(input("masukkan sisi per5segi: "))
+
+# luas_persegi(sisi)
+
+# #volume persegi
+# def volume_persegi(sisi):
+#     volume = sisi * sisi * sisi
+#     print(f"volume persegi = {volume}")
+    
+# sisi = int(input("masukkan sisi persegi: "))
+
+# volume_persegi(sisi)
+
+
+# def kali (a,b):
+#     hasil = a * b
+#     return hasil
+
+# print (kali(5,3))
+
+
+# def luas_persegi(sisi):
+#     luas = sisi * sisi
+#     return luas
+# # rumus: sisi x sisi x sisi
+# def volume_persegi(sisi):
+#     volume = luas_persegi(sisi) * sisi
+#     print ("volume persegi = ", volume)
+# # pemanggilan Fungsi
+#     volume_persegi(6)
+
+
+# a = int(input("nilai: "))
+# def penjumlahan(a):
+#     hasil= a + a
+#     return hasil
+# def perkalian(a):
+#     hasil= penjumlahan(a) * a
+#     print(f"hasil = {hasil}")
+# perkalian(a)
+
+
+# buku =[]
+# def show_data():
+# if len(buku) <= 0:
+# print ("Belum Ada data")
+# else:
+# print("ID", "Nama Buku")
+# for indeks in range(len(buku)):
+# print (indeks, buku[indeks])
+
+
+# # fungsi untuk menambah data
+# def insert_data():
+
+
+
+# buku_baru = input("Judul Buku : ")
+# buku.append(buku_baru)
+
+# # fungsi untuk edit data
+# def edit_data():
+# show_data()
+# indeks = int(input("Inputkan ID buku: "))
+# if(indeks >= len(buku) or indeks < 0):
+# print ("ID salah")
+# else:
+# judul_baru = input("Judul baru: ")
+# buku[indeks] = judul_baru
+
+# # fungsi untuk menghapus data
+# def delete_data():
+# show_data()
+# indeks = int(input("Inputkan ID buku: "))
+# if(indeks >= len(buku) or indeks < 0):
+# print ("ID salah")
+# else:
+# buku.remove(buku[indeks])
